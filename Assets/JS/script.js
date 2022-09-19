0afe55a988c7ca9d54828d83534feabc076e48c2
JamesJPorter <porterjjames@gmail.com>
2022-09-18 22:42:36 -0700

    trying to populate calendar

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,51 +1,62 @@
 var time = moment().format("LLLL");
-hourlyTime = moment("8 am", "hh a").format("h:mm");
+var startTime = 8;
+hourlyTime = moment(startTime, "hh a").format("h:mm");
 var timeLabel = $(".time");
-console.log(timeLabel)
-var calendarContainer = document.getElementById("calendar-content-container");
-console.log(calendarContainer)
-var calendarRow = document.createElement("form")
-// var calendarRow = $("form")
-calendarRow.setAttribute("class", "row time-block")
-var calendarTime = document.createElement("label");
-calendarTime.setAttribute("class", "col-2 hour");
-calendarTime.setAttribute("name", "time");
-calendarTime.setAttribute("for", "hr1")
-var calendarInfo = document.createElement("input");
-calendarInfo.setAttribute("class", "col-8 description textarea")
-calendarInfo.setAttribute("type", "text");
-calendarInfo.setAttribute("name", "hr1")
-var calendarSave = document.createElement("div");
-calendarSave.setAttribute("class", "col-2")
-var saveBtn = document.createElement("button")
-saveBtn.setAttribute("class", "saveBtn col-2")
-saveBtn.setAttribute("type", "submit")
+console.log(timeLabel);
+var calendarContainer = $("#calendar-content-container");
+console.log(calendarContainer);
+var calendarTime = $("<label></label>");
+calendarTime.attr({ class: "col-2 hour", name: "time", for: "hr1" });
+var calendarInfo = $("<input></input>");
+calendarInfo.attr({
+  class: "col-8 description textarea",
+  type: "text",
+  name: "hr1",
+});
+var calendarSave = $("<div></div>");
+calendarSave.attr("class", "col-2");
+var saveBtn = $("<button></button>");
+saveBtn.attr({ class: "saveBtn col-2", type: "submit" });
+saveBtn.attr("type", "submit");
 var timeIndex = 10;
+var calendarStructure = [];
 
 inIt();
-setInterval(clock, 1000)
+setInterval(clock, 1000);
 
 function inIt() {
-    $("#currentDay").text(time);
-    timeLabel.text(hourlyTime);
-    // console.log(timelabel);
-    populateCalendar();
+  $("#currentDay").text(time);
+  timeLabel.text(hourlyTime);
+  var initStorage = JSON.parse(localStorage.getItem("calendarStorage"));
+  if (initStorage != null) {
+    for (var i = 0; i < initStorage.length; i++) {
+      calendarStructure.push(initStorage[i]);
+      console.log(calendarStructure);
+    }
+  }
+  populateCalendar();
 }
 
 function clock() {
-    $("#currentDay").text(time);
-    time = moment().format("LLLL")
+  $("#currentDay").text(time);
+  time = moment().format("LLLL");
 }
 
-
+for (var i = 0; i < timeIndex; i++) {
+  calendarStructure.push(populateCalendar[i]);
+  localStorage.setItem("calendarStorage", JSON.stringify(calendarStructure));
+  console.log(calendarStructure);
+}
 
 function populateCalendar() {
-    for (var i = 0; i < timeIndex; i++)
-    calendarContainer.appendChild(calendarRow);
-    calendarRow.appendChild(calendarTime);
-    calendarTime.textContent = hourlyTime;
-    calendarRow.appendChild(calendarInfo);
-    calendarInfo.textContent = "WAKE UP! DONT SLEEP IN!"
-    calendarRow.appendChild(saveBtn);
-    // calendarSave.append(saveBtn);
+    for ( var i = 0; i < timeIndex; i++)
+  var calendarForm = $("<form></form>");
+  calendarForm.attr("class", "row time-block");
+  calendarContainer.append(calendarForm);
+  calendarForm.append(calendarTime);
+  startTime += i;
+  calendarTime.text(hourlyTime);
+  calendarForm.append(calendarInfo);
+  calendarInfo.val("WAKE UP! DONT SLEEP IN!");
+  calendarForm.append(saveBtn);
 }

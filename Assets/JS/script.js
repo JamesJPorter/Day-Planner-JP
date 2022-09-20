51cc9da909d1c9c9780b3e3dda9225101e803ead
JamesJPorter <porterjjames@gmail.com>
2022-09-20 01:19:17 -0700

    cannot sort how to populate with saved values

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -13,6 +13,7 @@ var calendarTime = $("<label></label>");
 var calendarForm = $("<form></form>");
 inIt();
 setInterval(clock, 1000);
+setInterval(colorCoding, 1000);
 
 function inIt() {
   $("#currentDay").text(time);
@@ -22,7 +23,6 @@ function inIt() {
   if (initStorage != null) {
     for (var i = 0; i < initStorage.length; i++) {
       calendarStructure.push(initStorage[i]);
-      console.log(calendarStructure);
     }
   }
   populateCalendar();
@@ -33,30 +33,24 @@ function clock() {
   time = moment().format("LLLL");
 }
 
-// for (var i = 0; i < timeIndex; i++) {
-//   calendarStructure.push(populateCalendar[i]);
-//   localStorage.setItem("calendarStorage", JSON.stringify(calendarStructure));
-//   console.log(calendarStructure);
-// }
-
 function colorCoding() {
-  $(".time-block").each(function(){
-    var currentHour = moment().hour()
-    var currentBlockHour = $(this).data().hour
-    console.log(currentBlockHour)
+  $(".time-block").each(function () {
+    var currentHour = moment().hour();
+    var currentBlockHour = $(this).data().hour;
+    // console.log(currentHour)
+    // console.log(currentBlockHour)
     if (currentHour == currentBlockHour) {
-      $(this).addClass("class", "present");
-      $(this).removeClass("")
+      $(this).children(".description").addClass("present");
+      $(this).children(".description").removeClass("future past");
     } else if (currentHour < currentBlockHour) {
-      $(this).addClass("class", "future");
-      console.log("we are in the future")
+      $(this).children(".description").addClass("future");
+      $(this).children(".description").removeClass("present past");
     } else {
-      $(this).addClass("class", "past");
-      console.log("we are in the past")
+      $(this).children(".description").addClass("past");
+      $(this).children(".description").removeClass("future present");
     }
-  })
-  }
-
+  });
+}
 
 function populateCalendar() {
   for (var i = 0; i < timeIndex; i++) {
@@ -66,17 +60,49 @@ function populateCalendar() {
     var calendarTime = $("<label></label>");
     var calendarInfo = $("<input></input>");
     var saveBtn = $("<button></button>");
+    var saveIcon = $("<i></i>");
+    if (calendarStructure) {
+      calendarStructure.forEach(function (entry, idx) {
+        if (calendarStructure[idx].hourIndex === startTime) {
+          calendarTime.attr({ class: "col-1 hour", name: "time", for: "hr1" });
+          calendarInfo.attr({
+            class: "col-10 description textarea",
+            type: "text",
+            name: "hr1",
+          });
+          saveBtn.attr({ class: "saveBtn col-1", type: "submit" });
+          calendarForm.attr("class", "row time-block");
+          calendarForm.attr("data-hour", startTime);
+          saveIcon.attr("class", "fas fa-save");
+
+          calendarContainer.append(calendarForm);
+          calendarTime.text(hourlyTime);
+          calendarForm.append(calendarTime);
+          calendarInfo.text(calendarStructure[idx].hourTask);
+          console.log(calendarStructure[idx].hourTask);
+          calendarForm.append(calendarInfo);
+          calendarForm.append(saveBtn);
+          saveBtn.append(saveIcon);
+        }
+      });
+    }
+    // var hourlyTime = moment(startTime, "hh a").format("h:mm a");
+    // var calendarForm = $("<form></form>");
+    // var calendarTime = $("<label></label>");
+    // var calendarInfo = $("<input></input>");
+    // var saveBtn = $("<button></button>");
+    // var saveIcon = $("<i></i>");
 
     calendarTime.attr({ class: "col-1 hour", name: "time", for: "hr1" });
     calendarInfo.attr({
       class: "col-10 description textarea",
-      type: "text",
+      type: "textarea",
       name: "hr1",
     });
     saveBtn.attr({ class: "saveBtn col-1", type: "submit" });
     calendarForm.attr("class", "row time-block");
-    calendarForm.attr("data-hour", startTime)
-    
+    calendarForm.attr("data-hour", startTime);
+    saveIcon.attr("class", "fas fa-save");
 
     calendarContainer.append(calendarForm);
     calendarTime.text(hourlyTime);
@@ -84,21 +110,37 @@ function populateCalendar() {
     calendarInfo.val("Your task here");
     calendarForm.append(calendarInfo);
     calendarForm.append(saveBtn);
+    saveBtn.append(saveIcon);
   }
-  colorCoding()
+  colorCoding();
 }
 
-$("#calendar-content-container").on("click", ".saveBtn", function(event) {
+$("#calendar-content-container").on("click", ".saveBtn", function (event) {
   event.preventDefault();
-  var calendarEntry = {
-    hourIndex: "",
-    hourTask: "",
-  };
-  console.log($(event.target).parent().data().hour)
-  calendarEntry.hourIndex = $(".hour").text();
-  // console.log(calendarEntry.hourIndex);
-  calendarEntry.hourTask = $(".textarea").val();
-  calendarStructure.push(calendarEntry);
-  // console.log(calendarStructure);
+
+  var thisHour = $(this).parent().data().hour;
+  var thisTask = $(this).siblings(".description").val();
+
+  var existingIndex;
+  console.log("calendarStructure", calendarStructure);
+  calendarStructure.forEach(function (entry, idx) {
+    console.log("entry", entry);
+    if (entry.hourIndex === thisHour) {
+      console.log("found it!");
+      existingIndex = idx;
+    }
+  });
+
+  console.log("existingIndex", existingIndex);
+  if (existingIndex) {
+    calendarStructure[existingIndex].hourTask = thisTask;
+  } else {
+    var calendarEntry = {
+      hourIndex: thisHour,
+      hourTask: thisTask,
+    };
+    calendarStructure.push(calendarEntry);
+  }
+
   localStorage.setItem("calendarStorage", JSON.stringify(calendarStructure));
 });

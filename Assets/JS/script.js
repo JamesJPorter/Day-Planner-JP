696f8f72e0bd7d066982bba974bb07e939a3b4cf
JamesJPorter <porterjjames@gmail.com>
2022-09-20 18:28:00 -0700

    final commit

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -55,49 +55,45 @@ function colorCoding() {
 function populateCalendar() {
   for (var i = 0; i < timeIndex; i++) {
     var startTime = i + 8;
-    if (calendarStructure) {
-      calendarStructure.forEach(function (entry, idx) {
-        if (entry[idx].hourIndex === startTime) {
-          calendarTime.attr({ class: "col-1 hour", name: "time", for: "hr1" });
-          calendarInfo.attr({
-            class: "col-10 description textarea",
-            type: "text",
-            name: "hr1",
-          });
-          var hourlyTimeEdit = moment(startTime, "hh a").format("h:mm a");
-          var calendarFormEdit = $("<form></form>");
-          var calendarTimeEdit = $("<label></label>");
-          var calendarInfoEdit = $("<input></input>");
-          var saveBtnEdit = $("<button></button>");
-          var saveIconEdit = $("<i></i>");
-          saveBtnEdit.attr({ class: "saveBtn col-1", type: "submit" });
-          calendarFormEdit.attr("class", "row time-block");
-          calendarFormEdit.attr("data-hour", startTime);
-          saveIconEdit.attr("class", "fas fa-save");
-
-          calendarContainer.append(calendarForm);
-          calendarTimeEdit.text(hourlyTimeEdit);
-          calendarFormEdit.append(calendarTimeEdit);
-          calendarInfoEdit.val(calendarStructure[idx].hourTask);
-          console.log(calendarStructure[idx].hourTask);
-          calendarFormEdit.append(calendarInfoEdit);
-          calendarFormEdit.append(saveBtnEdit);
-          saveBtnEdit.append(saveIconEdit);
-        }
-      });
-    }
     var hourlyTime = moment(startTime, "hh a").format("h:mm a");
     var calendarForm = $("<form></form>");
     var calendarTime = $("<label></label>");
     var calendarInfo = $("<input></input>");
     var saveBtn = $("<button></button>");
     var saveIcon = $("<i></i>");
+    if (calendarStructure) {
+      calendarStructure.forEach(function (entry, idx) {
+        if (entry.hourIndex === startTime) {
+          console.log(calendarStructure[idx].hourIndex)
+          console.log(entry)
+          calendarTime.attr({ class: "col-1 hour", name: "time", for: "hr1" });
+          calendarInfo.attr({
+            class: "col-10 description textarea",
+            type: "text",
+            name: "hr1",
+          });
+          saveBtn.attr({ class: "saveBtn col-1", type: "submit" });
+          calendarForm.attr("class", "row time-block");
+          calendarForm.attr("data-hour", startTime);
+          saveIcon.attr("class", "fas fa-save");
 
+          calendarContainer.append(calendarForm);
+          calendarTime.text(hourlyTime);
+          calendarForm.append(calendarTime);
+          calendarInfo.val(entry.hourTask);
+          console.log(entry);
+          calendarForm.append(calendarInfo);
+          calendarForm.append(saveBtn);
+          saveBtn.append(saveIcon);
+        }
+      });
+    }
     calendarTime.attr({ class: "col-1 hour", name: "time", for: "hr1" });
     calendarInfo.attr({
       class: "col-10 description textarea",
       type: "textarea",
       name: "hr1",
+      placeholder: "your task here"
     });
     saveBtn.attr({ class: "saveBtn col-1", type: "submit" });
     calendarForm.attr("class", "row time-block");
@@ -107,7 +103,6 @@ function populateCalendar() {
     calendarContainer.append(calendarForm);
     calendarTime.text(hourlyTime);
     calendarForm.append(calendarTime);
-    calendarInfo.val("Your task here");
     calendarForm.append(calendarInfo);
     calendarForm.append(saveBtn);
     saveBtn.append(saveIcon);
